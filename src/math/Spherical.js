9fb947b9704cc0b313a139fdb82fd0a2d7d7ebef
zhiheng.jiang <zhiheng.jiang@ttpai.cn>
2019-06-04 16:16:22 +0800

    Spherical

--- src/math/Spherical.js
+++ src/math/Spherical.js
@@ -1,6 +1,47 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const MathUtils_1 = require("./MathUtils");
 class Spherical {
+    constructor(radius = 1.0, phi = 0, theta = 0) {
+        this.radius = radius;
+        this.phi = phi;
+        this.theta = theta;
+    }
+    set(radius, phi, theta) {
+        this.radius = radius;
+        this.phi = phi;
+        this.theta = theta;
+        return this;
+    }
+    clone() {
+        return new Spherical().copy(this);
+    }
+    copy(other) {
+        this.radius = other.radius;
+        this.phi = other.phi;
+        this.theta = other.theta;
+        return this;
+    }
+    makeSafe() {
+        let EPS = 0.000001;
+        this.phi = MathUtils_1.MathUtils.clamp(this.phi, EPS, Math.PI - EPS);
+        return this;
+    }
+    setFromVector3(v) {
+        return this.setFromCartesianCoords(v.x, v.y, v.z);
+    }
+    setFromCartesianCoords(x, y, z) {
+        this.radius = Math.sqrt(x * x + y * y + z * z);
+        if (this.radius === 0) {
+            this.theta = 0;
+            this.phi = 0;
+        }
+        else {
+            this.theta = Math.atan2(x, z);
+            this.phi = Math.acos(MathUtils_1.MathUtils.clamp(y / this.radius, -1, 1));
+        }
+        return this;
+    }
 }
 exports.Spherical = Spherical;
 //# sourceMappingURL=Spherical.js.map
--- src/math/Spherical.ts
+++ src/math/Spherical.ts
@@ -1,3 +1,57 @@
-export class Spherical {
+import {MathUtils} from "./MathUtils";
+import {Vector3} from "./Vector3";
 
+export class Spherical {
+    constructor(public radius: number=1.0,public phi: number=0,public theta: number=0 ){
+
+    }
+
+    set( radius: number, phi: number, theta: number ): Spherical{
+        this.radius = radius;
+        this.phi = phi;
+        this.theta = theta;
+
+        return this;
+    }
+
+    clone(): Spherical{
+        return new Spherical().copy( this );
+    }
+
+    copy( other: Spherical ): Spherical{
+        this.radius = other.radius;
+        this.phi = other.phi;
+        this.theta = other.theta;
+
+        return this;
+    }
+
+    makeSafe(): Spherical{
+        let EPS = 0.000001;
+        this.phi=MathUtils.clamp(this.phi,EPS,Math.PI-EPS);
+
+        return this;
+    }
+
+    setFromVector3( v: Vector3 ): Spherical{
+        return this.setFromCartesianCoords( v.x, v.y, v.z );
+    }
+
+    private setFromCartesianCoords(x: number, y: number, z: number):Spherical {
+        this.radius = Math.sqrt( x * x + y * y + z * z );
+
+        if ( this.radius === 0 ) {
+
+            this.theta = 0;
+            this.phi = 0;
+
+        } else {
+
+            this.theta = Math.atan2( x, z );
+            this.phi = Math.acos( MathUtils.clamp( y / this.radius, - 1, 1 ) );
+
+        }
+
+        return this;
+    }
 }
