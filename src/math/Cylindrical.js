263c7dc2613ae77907293e29ad68ea0fbfd492f0
zhiheng.jiang <zhiheng.jiang@ttpai.cn>
2019-06-04 16:09:06 +0800

    Cylindral

--- src/math/Cylindrical.js
+++ src/math/Cylindrical.js
@@ -1,6 +1,35 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 class Cylindrical {
+    constructor(radius = 1.0, theta = 0, y = 0) {
+        this.radius = radius;
+        this.theta = theta;
+        this.y = y;
+    }
+    clone() {
+        return new Cylindrical().copy(this);
+    }
+    copy(other) {
+        this.radius = other.radius;
+        this.theta = other.theta;
+        this.y = other.y;
+        return this;
+    }
+    set(radius, theta, y) {
+        this.radius = radius;
+        this.theta = theta;
+        this.y = y;
+        return this;
+    }
+    setFromVector3(v) {
+        return this.setFromCartesianCoords(v.x, v.y, v.z);
+    }
+    setFromCartesianCoords(x, y, z) {
+        this.radius = Math.sqrt(x * x + z * z);
+        this.theta = Math.atan2(x, z);
+        this.y = y;
+        return this;
+    }
 }
 exports.Cylindrical = Cylindrical;
 //# sourceMappingURL=Cylindrical.js.map
--- src/math/Cylindrical.ts
+++ src/math/Cylindrical.ts
@@ -1,3 +1,40 @@
+import {Vector3} from "./Vector3";
+
 export class Cylindrical {
 
+    constructor(public radius: number = 1.0, public theta: number = 0, public y: number = 0) {
+
+    }
+
+    clone(): Cylindrical {
+        return new Cylindrical().copy( this );
+    }
+
+    copy(other: Cylindrical): Cylindrical{
+        this.radius = other.radius;
+        this.theta = other.theta;
+        this.y = other.y;
+
+        return this;
+    }
+
+    set(radius: number, theta: number, y: number): Cylindrical{
+        this.radius = radius;
+        this.theta = theta;
+        this.y = y;
+
+        return this;
+    }
+
+    setFromVector3(v: Vector3): Cylindrical{
+        return this.setFromCartesianCoords( v.x, v.y, v.z );
+    }
+
+    setFromCartesianCoords(x: number, y: number, z: number): Cylindrical{
+        this.radius = Math.sqrt( x * x + z * z );
+        this.theta = Math.atan2( x, z );
+        this.y = y;
+
+        return this;
+    }
 }
